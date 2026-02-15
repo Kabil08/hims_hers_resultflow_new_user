3e0ccdc4fcb74f2cb9de0f37cf1ab1f5abab2bbb
Kabil08 <kabil.02.2000@mail.com>
2026-02-16 05:26:08 +0530

    image url update mobile view- new

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FloatingChatButton from "@/components/FloatingChatButton";
 import ChatDialog from "@/components/ChatDialog";
+import { useIsMobile } from "@/hooks/use-mobile";
 import "./App.css";
 
-const HIMS_IMAGE_URL =
+const HIMS_DESKTOP_IMAGE_URL =
   "https://res.cloudinary.com/dsf1qebes/image/upload/v1771198953/Screenshot_2026-02-16_at_5.11.37_AM_bcqido.png";
+
+const HIMS_MOBILE_IMAGE_URL =
+  "https://res.cloudinary.com/dsf1qebes/image/upload/v1771199618/Screenshot_2026-02-16_at_5.23.05_AM_hgajcr.png";
 
 function HimsIframe() {
   const [isChatOpen, setIsChatOpen] = useState(false);
+  const isMobile = useIsMobile();
 
   return (
     <div className="fixed inset-0 w-screen h-screen overflow-hidden">
       <div className="relative w-full h-full z-0">
         <img
-          src={HIMS_IMAGE_URL}
+          src={isMobile ? HIMS_MOBILE_IMAGE_URL : HIMS_DESKTOP_IMAGE_URL}
           alt="Hims Website"
           className="w-full h-full object-cover object-top"
         />
